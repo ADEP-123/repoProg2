42681b3a1dcda128469a575d4de82fe07cb6658a
ADEP-123 <andep210399@gmail.com>
2024-03-25 15:03:57 -0500

    feat: :sparkles: parcial 1 todo

---
+++ js/components/p1.js
@@ -0,0 +1,18 @@
+const p1 = /*html*/`
+<h1>Parcial 1</h1>
+<h2>Desarrollo de aplicacion To Do</h2>
+<p id="enunciado">La aplicación consta de un TO DO, es decir, una herramienta para organizar tareas por cumplir, marcarlas como cumplidas y eliminarlas, para generar una mejor interacción con el usuario y un mayor sentido de personalización se agregó una sección para colocar un nombre de usuario, posteriormente una sección de carga de la aplicación que genera una sensación de mas solidez en la misma y finalmente el panel interactivo de la aplicación.</p>
+<div id=div_img_res>
+    <h5>Resultado</h5>
+    <p>A continuacion puede encontrar una pantalla para previsualizar la aplicacion, pero se recomienda manipularla desde el siguiente <a href="https://adep-123.github.io/ToDo/" target="_blank" >link</a></p>
+    <a></a>
+    <iframe
+        id="resultFrame"
+        width="1000"
+        height="700"
+        src="https://adep-123.github.io/ToDo/">
+    </iframe>
+</div>
+
+`;
+export default p1
