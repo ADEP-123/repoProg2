dad1338ea8c35ac7e7405105ec53bf1e4e4a8a3f
ADEP-123 <andep210399@gmail.com>
2024-03-25 15:13:25 -0500

    fix: :art: agregando espacios para mejorar estetica en ejercicios

--- js/components/analCron.js
+++ js/components/analCron.js
@@ -3,15 +3,16 @@ const analCron = /*html*/`
 <h2>Desarrollo de aplicacion Cronometro Analogo</h2>
 <p id="enunciado">La aplicación consta de un cronometro con precisión de 10 milisegundos, con dos botones, uno que pausa o reanuda el conteo, y otro que lo reinicia, además debajo de cronometro se puso un contador para tener más claridad en el tiempo medido.</p>
 <div id=div_img_res>
-    <h5>Resultado</h5>
+    <br><br>
+    <h3>Resultado</h3>
     <p>A continuacion puede encontrar una pantalla para previsualizar la aplicacion, pero se recomienda manipularla desde el siguiente <a href="https://adep-123.github.io/CronometroAnalogo/" target="_blank" >link</a></p>
-    <a></a>
     <iframe
         id="resultFrame"
         width="1000"
         height="700"
         src="https://adep-123.github.io/CronometroAnalogo/">
     </iframe>
+    <br>
     <p>El link del repositorio del proyecto lo puede encontrar en el siguiente <a href="https://github.com/ADEP-123/CronometroAnalogo" target="_blank" >link</a></p>
 </div>
 
--- js/components/p1.js
+++ js/components/p1.js
@@ -3,15 +3,16 @@ const p1 = /*html*/`
 <h2>Desarrollo de aplicacion To Do</h2>
 <p id="enunciado">La aplicación consta de un TO DO, es decir, una herramienta para organizar tareas por cumplir, marcarlas como cumplidas y eliminarlas, para generar una mejor interacción con el usuario y un mayor sentido de personalización se agregó una sección para colocar un nombre de usuario, posteriormente una sección de carga de la aplicación que genera una sensación de mas solidez en la misma y finalmente el panel interactivo de la aplicación.</p>
 <div id=div_img_res>
-    <h5>Resultado</h5>
+    <br><br>
+    <h3>Resultado</h3>
     <p>A continuacion puede encontrar una pantalla para previsualizar la aplicacion, pero se recomienda manipularla desde el siguiente <a href="https://adep-123.github.io/ToDo/" target="_blank" >link</a></p>
-    <a></a>
     <iframe
         id="resultFrame"
         width="1000"
         height="700"
         src="https://adep-123.github.io/ToDo/">
     </iframe>
+    <br>
     <p>El link del repositorio del proyecto lo puede encontrar en el siguiente <a href="https://github.com/ADEP-123/ToDo" target="_blank" >link</a></p>
 </div>
 
