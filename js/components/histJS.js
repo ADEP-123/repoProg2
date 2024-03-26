b24688925d1975e4072df4035906a527c2b40c0f
ADEP-123 <andep210399@gmail.com>
2024-03-26 12:22:47 -0500

    feat: :sparkles: nueva actividad corte 2 act 2

---
+++ js/components/histJS.js
@@ -0,0 +1,20 @@
+const histJS = /*html*/`
+<h1>Actividad 2 Corte 2</h1>
+<h2>Conteo Regresivo con historia java Script</h2>
+<p id="enunciado">La aplicación consta de un panel inicial que le pregunta al usuario el tiempo del cual desea hacer el conteo regresivo posteriormente se despliega el conteo y al finalizar este una breve historia de JavaScript</p>
+<div id=div_img_res>
+    <br><br>
+    <h3>Resultado</h3>
+    <p>A continuacion puede encontrar una pantalla para previsualizar la aplicacion, pero se recomienda manipularla desde el siguiente <a href="https://adep-123.github.io/historJs/" target="_blank" >link</a></p>
+    <iframe
+        id="resultFrame"
+        width="1000"
+        height="700"
+        src="https://adep-123.github.io/historJs/">
+    </iframe>
+    <br>
+    <p>El link del repositorio del proyecto lo puede encontrar en el siguiente <a href="https://github.com/ADEP-123/historJs" target="_blank" >link</a></p>
+</div>
+
+`;
+export default histJS
