e4efaa950832d09bdc48a5ad8f7119e1dac8f790
ADEP-123 <andep210399@gmail.com>
2024-04-09 11:20:03 -0500

    feat: :sparkles: app del clima

---
+++ js/components/clima.js
@@ -0,0 +1,20 @@
+const clima = /*html*/`
+<h1>Actividad 1 Corte 2</h1>
+<h2>Desarrollo de aplicacion Cronometro Analogo</h2>
+<p id="enunciado">La aplicacion consta de un panel del clima, donde se puede buscar por pais y ciudad o buscar directamente en su ubicacion, dando click al logo del clima actual podra desplegar ademas informacion acerca de la sensacion termica</p>
+<div id=div_img_res>
+    <br><br>
+    <h3>Resultado</h3>
+    <p>A continuacion puede encontrar una pantalla para previsualizar la aplicacion, pero se recomienda manipularla desde el siguiente <a href="https://adep-123.github.io/appClima/" target="_blank" >link</a></p>
+    <iframe
+        id="resultFrame"
+        width="1000"
+        height="700"
+        src="https://adep-123.github.io/appClima/">
+    </iframe>
+    <br>
+    <p>El link del repositorio del proyecto lo puede encontrar en el siguiente <a href="https://github.com/ADEP-123/appClima" target="_blank" >link</a></p>
+</div>
+
+`;
+export default clima
